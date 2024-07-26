c972a08508f1e7259304dc149064dcb2e8b7eabc
zevskiel <chandra.zev@gmail.com>
2024-07-26 16:30:20 +0700

    remove unused files

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -15,29 +15,4 @@ class UserRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Users::class);
     }
-
-    //    /**
-    //     * @return Project[] Returns an array of Project objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Project
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
